fb3de3aa83743d55dd0da34813dc5ccf84b66a89
s4916759 <s4916759@bournemouth.ac.uk>
2020-05-07 12:07:44 +0100

    Reverted back to local state

--- src/containers/Calendar/Calendar.js
+++ src/containers/Calendar/Calendar.js
@@ -2,57 +2,41 @@ import React, { Component } from "react";
 import Row from "../../components/Row/Row";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import classes from "../Calendar/Calendar.module.css";
-// import { calendarsRef } from "../../firebase";
-import axios from "axios";
-import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Calendar extends Component {
-	// state = {
-	// 	currentCalendar: {
-	// 		cards: {
-	// 			"card-1": { id: "card-1", content: "Instagram - 15:00pm" },
-	// 			"card-2": { id: "card-2", content: "Facebook - 16:00pm" },
-	// 			"card-3": { id: "card-3", content: "Instagram - 17:00pm" },
-	// 			"card-4": { id: "card-4", content: "Twitter - 20:00pm" },
-	// 		},
-	// 		rows: {
-	// 			"row-1": {
-	// 				id: "row-1",
-	// 				title: "Monday 4th May",
-	// 				cardIds: ["card-1", "card-2", "card-3", "card-4"],
-	// 			},
-	// 			"row-2": {
-	// 				id: "row-2",
-	// 				title: "Tuesday 5th May",
-	// 				cardIds: [],
-	// 			},
-	// 			"row-3": {
-	// 				id: "row-3",
-	// 				title: "Thursday 7th May",
-	// 				cardIds: [],
-	// 			},
-	// 			"row-4": {
-	// 				id: "row-4",
-	// 				title: "Friday 8th May",
-	// 				cardIds: [],
-	// 			},
-	// 		},
-	// 		rowOrder: ["row-1", "row-2", "row-3", "row-4"],
-	// 	},
-	// };
-
 	state = {
-		currentCalendar: null,
+		currentCalendar: {
+			cards: {
+				"card-1": { id: "card-1", content: "Instagram - 15:00pm" },
+				"card-2": { id: "card-2", content: "Facebook - 16:00pm" },
+				"card-3": { id: "card-3", content: "Instagram - 17:00pm" },
+				"card-4": { id: "card-4", content: "Twitter - 20:00pm" },
+			},
+			rows: {
+				"row-1": {
+					id: "row-1",
+					title: "Monday 4th May",
+					cardIds: ["card-1", "card-2", "card-3", "card-4"],
+				},
+				"row-2": {
+					id: "row-2",
+					title: "Tuesday 5th May",
+					cardIds: [],
+				},
+				"row-3": {
+					id: "row-3",
+					title: "Thursday 7th May",
+					cardIds: [],
+				},
+				"row-4": {
+					id: "row-4",
+					title: "Friday 8th May",
+					cardIds: [],
+				},
+			},
+			rowOrder: ["row-1", "row-2", "row-3", "row-4"],
+		},
 	};
-
-	componentDidMount() {
-		axios
-			.get("https://stacker-rdb.firebaseio.com/calendars/Test.json")
-			.then((response) => {
-				this.setState({ currentCalendar: response.data });
-			})
-			.catch((error) => {});
-	}
 
 	onDragEnd = (result) => {
 		const { destination, source, draggableId, type } = result;
@@ -69,12 +53,12 @@ class Calendar extends Component {
 		}
 
 		if (type === "row") {
-			const newRowOrder = Array.from(this.state.currentCalendar.rowOrder);
+			const newRowOrder = Array.from(this.state.rowOrder);
 			newRowOrder.splice(source.index, 1);
 			newRowOrder.splice(destination.index, 0, draggableId);
 
 			const newState = {
-				...this.state.currentCalendar,
+				...this.state,
 				rowOrder: newRowOrder,
 			};
 
@@ -131,36 +115,29 @@ class Calendar extends Component {
 	};
 
 	render() {
-		console.log("Start of render", this.state.currentCalendar);
-
-		let calendar = <Spinner />;
-
-		if (this.state.currentCalendar !== null) {
-			calendar = (
-				<DragDropContext onDragEnd={this.onDragEnd}>
-					<Droppable droppableId="all-rows" direction="vertical" type="row">
-						{(provided) => (
-							<div
-								className={classes.calendarContainer}
-								{...provided.droppableProps}
-								ref={provided.innerRef}>
-								{this.state.currentCalendar.rowOrder.map((rowId, index) => {
-									const row = this.state.currentCalendar.rows[rowId];
-									const cards = row.cardIds.map(
-										(cardId) => this.state.currentCalendar.cards[cardId]
-									);
-									return (
-										<Row key={row.id} row={row} cards={cards} index={index} />
-									);
-								})}
-								{provided.placeholder}
-							</div>
-						)}
-					</Droppable>
-				</DragDropContext>
-			);
-		}
-		return calendar;
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				<Droppable droppableId="all-rows" direction="vertical" type="row">
+					{(provided) => (
+						<div
+							className={classes.calendarContainer}
+							{...provided.droppableProps}
+							ref={provided.innerRef}>
+							{this.state.currentCalendar.rowOrder.map((rowId, index) => {
+								const row = this.state.currentCalendar.rows[rowId];
+								const cards = row.cardIds.map(
+									(cardId) => this.state.currentCalendar.cards[cardId]
+								);
+								return (
+									<Row key={row.id} row={row} cards={cards} index={index} />
+								);
+							})}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
+		);
 	}
 }
 
